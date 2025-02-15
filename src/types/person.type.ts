d9ef3641e782c039e574a45fa93c0edb1376a82a
Sergio Reyes <sergio.reyes@plexusworldwide.com>
2025-02-15 01:06:31 -0600

    1. Testing

---
+++ src/types/person.type.ts
@@ -0,0 +1,15 @@
+interface BaseRecord {
+  id: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface PersonType extends BaseRecord {
+  curp: string;
+  lastName: string;
+  name: string;
+  nss: string;
+  rfc: string;
+  nationality: string;
+}
+
